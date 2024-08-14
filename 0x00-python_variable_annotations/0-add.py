4f57ac4497783144438ad667b76a2d75c22eb3c1
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-08-14 11:14:31 +0100

    (fix): Basic annotation - add
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,2 +1,7 @@
+#!/usr/bin/env python3
+""" Module - Basic annotations - add  """
+
+
 def add(a: float, b: float) -> float:
+    """ Function that returns the sum of two floats """
     return a + b
