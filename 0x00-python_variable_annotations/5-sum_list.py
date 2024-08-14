4477e059ae9912d89246978ba66d6cfac4078dca
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-08-14 12:02:57 +0100

    (fix): complex types - list of flats using typing.List
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Module that contains the function sum_list"""
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """Function that takes a list of floats as input and
     returns the sum of all elements in the list"""
     return sum(input_list)
