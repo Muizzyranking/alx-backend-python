43b7622292d9bfe88aaa0709a32ee93e64dd1ddd
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-08-14 11:50:04 +0100

    Basic annotation - floor
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""""Module for the floor function"""
+
+
+def floor(n: float) -> int:
+    """Function that takes a float n as argument and returns the
+    floor of the float."""
+    return int(n)
